c865239b2953546915f4a0d09a1ed95aada48d37
mahmudul-hasan01 <mahamudul2018@gmail.com>
2024-06-13 22:58:56 +0600

    company name and footer is shown

--- src/MainLayout/MainLayout.jsx
+++ src/MainLayout/MainLayout.jsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../Components/Navbar/Navbar";
 import './layOut.css'
+import Footer from "../Components/Footer/Footer";
 
 const MainLayout = () => {
     return (
         <div className="xl:max-w-screen-xl max-w-screen-2xl mx-auto">
             <Navbar></Navbar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
